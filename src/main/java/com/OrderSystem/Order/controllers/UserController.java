34520731e6fa0e6e9ff0c500a4b6a4c56e08ae3a
Isaquesantos7 <Isaquesantos.second.1998@gmail.com>
2024-05-31 17:44:41 -0300

    feat: repository and saving user

--- src/main/java/com/OrderSystem/Order/controllers/UserController.java
+++ src/main/java/com/OrderSystem/Order/controllers/UserController.java
@@ -1,15 +1,28 @@
 package com.OrderSystem.Order.controllers;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.OrderSystem.Order.DTOS.UserDTO;
 import com.OrderSystem.Order.entities.User;
+import com.OrderSystem.Order.repositories.UserRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class UserController {
-
+	
+	private UserRepository userRepository;
+	
+	public UserController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
 	@GetMapping("/api/users")
 	public ResponseEntity<User> findAll() {
 		User user = new User(1L, "Isaque", "isaquesantos.1998@gmail.com", "+5571987308769", "86992134");
@@ -17,4 +30,12 @@ public class UserController {
 		return ResponseEntity.status(HttpStatus.OK).body(user);
 	}
 	
+	@PostMapping("/api/users")
+	public ResponseEntity<User> saveUser(@RequestBody @Valid UserDTO userDTO) {
+		User user = new User();
+		
+		BeanUtils.copyProperties(userDTO, user);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.userRepository.save(user));
+	}
 }
