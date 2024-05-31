4c637e80afdfffe0d1b054d6c3edd5521235b5bc
Isaquesantos7 <Isaquesantos.second.1998@gmail.com>
2024-05-31 16:08:01 -0300

    feat: created project

---
+++ src/main/java/com/OrderSystem/Order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.OrderSystem.Order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
